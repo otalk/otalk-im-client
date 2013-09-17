f122a8f446d1512c1a8d3d03c2808a8c217bd7ea
Lance Stout <lancestout@gmail.com>
2013-09-17 01:24:10 -0700

    Fix url for otalk.im websocket endpoint

--- public/js/oauthLogin.js
+++ public/js/oauthLogin.js
@@ -14,7 +14,7 @@ parts.forEach(function (value) {
                 localStorage.config = JSON.stringify({
                     jid: user.username.toLowerCase() + "@otalk.im",
                     server: "otalk.im",
-                    wsURL: "wss://otalk.im/xmpp-websocket",
+                    wsURL: "wss://otalk.im:5281/xmpp-websocket",
                     credentials: {
                         username: user.username.toLowerCase(),
                         password: token
