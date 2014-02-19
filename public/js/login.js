7c9b30831d720c6b4c6e51f7d3725be3c9820f99
Lance Stout <lancestout@gmail.com>
2014-02-18 20:44:15 -0800

    Enable BOSH!

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,28 @@
 $('#loginbox form').on('submit', function (e) {
     var jid = $('#jid').val();
     var password = $('#password').val();
-    var wsURL = $('#wsURL').val();
+    var connURL = $('#connURL').val();
+
+    var wsURL = '';
+    var boshURL = '';
+    if (connURL.indexOf('http') == 0) {
+        boshURL = connURL;
+    } else {
+        wsURL = wsURL;
+    }
 
     localStorage.config = JSON.stringify({
         jid: jid,
         server: jid.slice(jid.indexOf('@') + 1),
         wsURL: wsURL,
+        boshURL: boshURL,
         credentials: {
             password: password
         }
     });
 
     window.location = '/';
-    
+
     e.preventDefault();
     return false;
 });
