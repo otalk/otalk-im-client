29ec69dfb2d890a2f78b747b298523423071b458
Lance Stout <lancestout@gmail.com>
2014-02-24 13:54:08 -0800

    Actually use the given urls

--- public/js/login.js
+++ public/js/login.js
@@ -3,12 +3,15 @@ $('#loginbox form').on('submit', function (e) {
     var password = $('#password').val();
     var connURL = $('#connURL').val();
 
+    var transport;
     var wsURL = '';
     var boshURL = '';
-    if (connURL.indexOf('http') == 0) {
+    if (connURL.indexOf('http') === 0) {
         boshURL = connURL;
+        transport = 'bosh';
     } else {
         wsURL = wsURL;
+        transport = 'websocket';
     }
 
     localStorage.config = JSON.stringify({
@@ -16,6 +19,7 @@ $('#loginbox form').on('submit', function (e) {
         server: jid.slice(jid.indexOf('@') + 1),
         wsURL: wsURL,
         boshURL: boshURL,
+        transport: transport,
         credentials: {
             password: password
         }
